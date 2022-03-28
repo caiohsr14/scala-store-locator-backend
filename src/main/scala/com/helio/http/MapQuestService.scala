5e9c6842ec5a9f97df701ae42814fed6f5d428bc
Caio Rodrigues <caiohsr__@hotmail.com>
2022-03-28 02:29:53 -0300

    Remove secondary unused main

--- src/main/scala/com/helio/http/MapQuestService.scala
+++ src/main/scala/com/helio/http/MapQuestService.scala
@@ -2,21 +2,22 @@ package com.helio.http
 
 import cats.effect.IO
 import cats.effect.unsafe.implicits.global
-import org.http4s.circe.jsonOf
-import org.http4s.ember.client._
-import org.http4s.client._
 import io.circe.generic.auto._
 import org.http4s.EntityDecoder
+import org.http4s.circe.jsonOf
+import org.http4s.client._
+import org.http4s.ember.client._
 import org.log4s.{Logger, getLogger}
 
-import scala.concurrent.duration._
 import java.util.concurrent._
+import scala.concurrent.duration._
 
 object MapQuestService {
 
   private val log: Logger = getLogger
 
-  private implicit val mapQuestDecoder: EntityDecoder[IO, MapQuestResponse] = jsonOf[IO, MapQuestResponse]
+  private implicit val mapQuestDecoder: EntityDecoder[IO, MapQuestResponse] =
+    jsonOf[IO, MapQuestResponse]
 
   EmberClientBuilder.default[IO].build.use { client =>
     // use `client` here and return an `IO`.
@@ -34,19 +35,14 @@ object MapQuestService {
    */
   def geocode(zipCode: String): Option[MapQuestResponse] = {
     Config.MQ_API_KEY match {
-      case Some(key)=>
-        val helloJames = httpClient.expect[MapQuestResponse](s"https://www.mapquestapi.com/geocoding/v1/address?key=$key&location=${zipCode}")
+      case Some(key) =>
+        val helloJames = httpClient.expect[MapQuestResponse](
+          s"https://www.mapquestapi.com/geocoding/v1/address?key=$key&location=${zipCode}"
+        )
         helloJames.unsafeRunTimed(5000.seconds)
       case None =>
         log.error("No Map Quest Key Set!")
         None
     }
-  }
-}
-
-// Sample Run.
-object MapQuestTest {
-  def main(args: Array[String]): Unit = {
-    println(MapQuestService.geocode(zipCode = "01742"))
   }
 }
