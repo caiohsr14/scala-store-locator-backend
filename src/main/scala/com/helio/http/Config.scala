c496913898a974464bb6ed489d944542f6b62e8b
Caio Rodrigues <caiohsr__@hotmail.com>
2022-03-28 01:27:43 -0300

    Add scalafmt conf

--- src/main/scala/com/helio/http/Config.scala
+++ src/main/scala/com/helio/http/Config.scala
@@ -6,7 +6,8 @@ import com.typesafe.config._
 object Config {
   val base = ConfigFactory.load().getConfig("store-locator")
   val env = Option(System.getenv("ENVIRONMENT")).getOrElse("dev")
-  val config = if (base.hasPath(env)) base.getConfig(env).withFallback(base) else base
+  val config =
+    if (base.hasPath(env)) base.getConfig(env).withFallback(base) else base
 
   lazy val MQ_API_KEY: Option[String] = {
     Option(config.getString("mq-api-key"))
